ea22a8124438572b065900cd3941ac9f80bd6488
Fellah <abdoulazizdiallofouta@gmail.com>
2022-07-25 16:13:18 +0200

    Changing the port

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.post("/", function (req, res) {
   request.end();
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Started to listen on port 3000");
 });
 
